0c8c087f8ec618cc7dca811c6f19c87d48e46bf3
dinvine <dinvine@qq.com>
2019-12-12 18:59:06 +1300

    NUnitDemo

---
+++ NUnitDemoProject/EmployeeDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NUnitDemoProject
+{
+    class EmployeeDetails
+    {
+        public int id
+        {
+            get;
+            set;
+        }
+        public string name
+        {
+            get;
+            set;
+        }
+        public double salary
+        {
+            get;
+            set;
+        }
+        public string gender
+        {
+            get;
+            set;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
